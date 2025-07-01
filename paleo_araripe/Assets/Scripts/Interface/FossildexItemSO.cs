25c8ba6dfa9b797f0bb19b16f9803d4291ba3594
TewRocha16 <mateusro.fonseca@gmail.com>
2025-06-30 23:48:32 -0300

    feat: configuracao prefab

--- paleo_araripe/Assets/Scripts/Interface/FossildexItemSO.cs
+++ paleo_araripe/Assets/Scripts/Interface/FossildexItemSO.cs
@@ -9,10 +9,12 @@ namespace PaleoAraripe
     [CreateAssetMenu(fileName = "ItemFossildex", menuName = "Fossildex")]
     public class FossildexItemSO : ScriptableObject
     {
+        [SerializeField] private int fossilID;
         [SerializeField] private string titulo;
         [SerializeField] private string descricao;
         [SerializeField] private Sprite imagem;
 
+        public int FossilID => fossilID;
         public string Titulo => titulo;
         public string Descricao => descricao;
         public Sprite Imagem => imagem;
