a5a78d6d6580ec2f86163d1f0802ba9a9cb6b552
TewRocha16 <mateusro.fonseca@gmail.com>
2025-07-02 00:43:48 -0300

    fix tutorial pos merge

--- paleo_araripe/Assets/Scripts/Interface/Menu/MenuPrincipal.cs
+++ paleo_araripe/Assets/Scripts/Interface/Menu/MenuPrincipal.cs
@@ -8,6 +8,7 @@ namespace PaleoAraripe {
     {
         [SerializeField] private GameObject telaPrincipal;
         [SerializeField] private GameObject telaSobre;
+        [SerializeField] private GameObject telaTutorial;
         [SerializeField] private int menuFrameRate = 60;
         [SerializeField] private Animator animacaoCarregamento;
 
@@ -27,9 +28,19 @@ namespace PaleoAraripe {
             telaPrincipal.SetActive(false);
             telaSobre.SetActive(true);
         }
+        public void AbrirTutorial()
+        {
+            telaPrincipal.SetActive(false);
+            telaTutorial.SetActive(true);
+        }
         public void FecharSobre()
         {
             telaSobre.SetActive(false);
+            telaPrincipal.SetActive(true);
+        }
+        public void FecharTutorial()
+        {
+            telaTutorial.SetActive(false);
             telaPrincipal.SetActive(true);
         }
     }
