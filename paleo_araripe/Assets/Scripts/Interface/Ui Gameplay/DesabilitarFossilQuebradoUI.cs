cdf9bb6b713509102b2a91de626f6687ca4011d8
TewRocha16 <mateusro.fonseca@gmail.com>
2025-09-03 01:56:31 -0300

    fixes quase finais

--- paleo_araripe/Assets/Scripts/Interface/Ui Gameplay/DesabilitarFossilQuebradoUI.cs
+++ paleo_araripe/Assets/Scripts/Interface/Ui Gameplay/DesabilitarFossilQuebradoUI.cs
@@ -6,6 +6,11 @@ namespace PaleoAraripe
 {
     public class DesabilitarFossilQuebradoUI : MonoBehaviour
     {
+        [SerializeField] private AudioClip sfx;
+        private void OnEnable()
+        {
+            UtilitarioAudio.TocarSFX(sfx);
+        }
         public void DisableUIFeedback()
         {
             gameObject.SetActive(false);
